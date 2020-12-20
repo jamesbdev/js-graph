91fad2e674fd5af8daca76166896f643a2ca01b0
jaybaybremner <j.bremner@ymail.com>
2020-12-20 16:49:04 +0000

    first commit

---
+++ script.js
@@ -0,0 +1,41 @@
+const ctx = document.getElementById('myChart');
+// const myBarChart = new Chart(ctx, {
+//     type: 'horizontalBar',
+//     data: [70.16, 64.55, 53.45, 49.65, 47.97, 41.21, 31.59, 25.26, 22.33, 17.67],
+//     options: {
+
+//     }
+// });
+//example
+var myChart = new Chart(ctx, {
+    type: 'horizontalBar',
+    data: {
+        
+        labels: ['UK', 'Peru', 'Chile', 'US', 'Brazil', 'Mexico', 'Bolivia', 'Columbia', 'Iran', 'South Africa'],
+        datasets: [{
+            label: 'Death per 100,000 habitants',
+            data: [70.16, 64.55, 53.45, 49.65, 47.97, 41.21, 31.59, 25.26, 22.33, 17.67],
+            backgroundColor: [
+                'rgba(229, 1, 37, 1)',
+                'rgba(229, 1, 37, 1)',
+                'rgba(229, 1, 37, 1)',
+                'rgba(229, 1, 37, 1)',
+                'rgba(229, 1, 37, 1)',
+                'rgba(229, 1, 37, 1)',
+                'rgba(229, 1, 37, 1)',
+                'rgba(229, 1, 37, 1)',
+                'rgba(229, 1, 37, 1)',
+                'rgba(229, 1, 37, 1)'
+            ]
+        }]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true
+                }
+            }]
+        }
+    }
+});
